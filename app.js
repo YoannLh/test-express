d062da81c37585986b90484a53e291a4e20a8015
L'hostis <yolhostis@live.fr>
2022-04-27 01:37:43 +0200

    Ajout  CORS

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
     next()
 })
 
-app.use((req, res, next) => {
-    res.status(201)
-    next()
-})
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' })
-    next()
-})
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !')
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        { id: 'fdd', title: 'xvcvx', price: '23244' },
+        { id: 'fdd', title: 'xvcvx', price: '23244' },
+    ]
+    res.status(200).json(stuff)
 })
 
 module.exports = app
