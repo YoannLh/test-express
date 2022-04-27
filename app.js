3552696e049319f26f687f87702c7665fe3e9b7e
L'hostis <yolhostis@live.fr>
2022-04-27 03:32:31 +0200

    Co avec mongoDB et schéma mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose
+    .connect(
+        'mongodb+srv://test-express:test-express@cluster0.yzgj6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 app.use(express.json())
 
