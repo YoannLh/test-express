6eb0b22199ccf3b66b2854687de4509a024fe13f
L'hostis <yolhostis@live.fr>
2022-04-29 14:55:13 +0200

    Ajout routes put et delete

--- app.js
+++ app.js
@@ -37,8 +37,12 @@ app.post('/api/stuff', (req, res, next) => {
         .save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
         .catch((error) => res.status(400).json({ error }))
-    //next()
-    console.log(thing)
+})
+
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch((error) => res.status(400).json({ error }))
 })
 
 app.get('/api/stuff', (req, res, next) => {
@@ -51,6 +55,12 @@ app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
         .then((thing) => res.status(200).json(thing))
         .catch((error) => res.status(404).json({ error }))
+})
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+        .catch((error) => res.status(400).json({ error }))
 })
 
 module.exports = app
